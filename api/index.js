24d340763c9b148eb2465c91f909fa21ae9b96ad
surender18 <surenderempire68@gmail.com>
2024-04-14 00:04:48 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(
     process.env.MONGO
@@ -13,8 +13,12 @@ mongoose.connect(
         console.log(err);
     });
 const app= express();
+
+app.use(express.json());
+
 app.listen (3000,()=>{
     console.log("server is running at 3000");
 })
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
