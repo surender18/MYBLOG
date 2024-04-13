27bb6ae60de03d0344364053c90bdf35ad2192ec
surender18 <surenderempire68@gmail.com>
2024-04-13 18:12:40 +0530

    Remove unnecessary files ,create and run server

--- BlogMingle/src/components/Header.jsx
+++ BlogMingle/src/components/Header.jsx
@@ -27,12 +27,12 @@ export default function Header() {
         <FaMoon />
       </Button>
       <Link to='/sign-in'>
-        <Button  gradientDuoTone='purpleToBlue'>
+        <Button  gradientDuoTone='purpleToBlue ' outline>
           Sign In
 
         </Button>
       </Link>
-      <NavbarToggle/>
+      <Navbar.Toggle/>
       </div>
       
       <Navbar.Collapse>
