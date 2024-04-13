809f704ce97e46080f86a024be218622720bd113
surender18 <surenderempire68@gmail.com>
2024-04-13 14:18:50 +0530

    create header page

--- BlogMingle/src/App.jsx
+++ BlogMingle/src/App.jsx
@@ -5,11 +5,13 @@ import Signin from './pages/Signin'
 import SignUP from './pages/SignUP'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
    
       <BrowserRouter >
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About />} />
